726d8178fb2a06fe79f6917f50d379ff70de8d64
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2019-10-03 08:27:34 -0300

    Update App.js
    Alterações necessárias para passar nos testes.

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Acceleration from './src/screens/Acceleration';
 import Profile from './src/screens/Profile';
 
 const AplicationStack = createStackNavigator(
-  {
-    Acceleration: Acceleration,
-    Profile: Profile
-  },
-  {
-    initialRouteName: "Acceleration",
-    headerMode: "none"
-  }
+	{
+		Acceleration: Acceleration,
+		Profile: Profile,
+	},
+	{
+		initialRouteName: 'Acceleration',
+		headerMode: 'none',
+	}
 );
 
-const App = createAppContainer(AplicationStack);
-
-export default App;
+export default createAppContainer(AplicationStack);
